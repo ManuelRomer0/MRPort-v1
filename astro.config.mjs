e9aa4def37fccf20de4f0d91a32917bed5442cdf
Manuel Romero <manuelromeromoreno13@gmail.com>
2025-12-19 23:30:53 -0500

    fixed deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://ManuelRomer0.github.io/Portfolio-v1',
-  base: ''
+  site: 'https://ManuelRomer0.github.io',
+  base: '/MRPort-v1'
 });
